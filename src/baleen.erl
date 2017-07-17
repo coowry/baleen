524ea3b57b5b0851c7f50a5ae94dcead5a6ba510
MiguelERuiz <meruiz77@gmail.com>
2017-07-17 19:04:21 +0200

    types validator and validator_result created

--- src/baleen.erl
+++ src/baleen.erl
@@ -1,12 +1,27 @@
 -module(baleen).
 
 %% API exports
--export([]).
+-export([fail/0, success/0]).
 
+-type validator_result(R) :: {ok, R} | {error, binary()} | boolean().
+
+-type validator(A,B) :: fun((A) -> validator_result(B)).
 %%====================================================================
 %% API functions
 %%====================================================================
+-spec fail() -> validator(_,_).
+fail() ->
+    fun(_) -> {error,<<"Fail validator">>} end.
 
+-spec success() -> validator(_,_).
+success() ->
+    fun(X) -> {ok, X} end.
+
+-spec chain(non_empty_list(validator(A,A))) -> validator(A,A).
+
+
+-spec all()
+    
 
 %%====================================================================
 %% Internal functions
