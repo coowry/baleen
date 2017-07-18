00f456c317f61f35219db549dcbcc68bf9d80acc
Ángel Herranz <aherranz@fi.upm.es>
2017-07-18 11:02:34 +0200

    New tests for member.

--- test/prop_complex_validation.erl
+++ test/prop_complex_validation.erl
@@ -1,7 +1,7 @@
 -module(prop_complex_validation).
 -include_lib("proper/include/proper.hrl").
 
--import(baleen, [validate/2, member/1, is_ok/1]).
+-import(baleen, [validate/2, member/1, is_ok/1, invalid/0]).
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
@@ -9,3 +9,7 @@
 prop_member() ->
   ?FORALL({L, T}, {list(term()), term()},
           lists:member(T,L) == is_ok(validate(member(L),T))).
+
+prop_empty_member() ->
+  ?FORALL(T, term(),
+          validate(member([]),T) == validate(invalid(),T)).
