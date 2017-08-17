c82c77c62f6700ce9ecfd4bffb781712ab2c6cc0
Ángel Herranz <aherranz@fi.upm.es>
2017-08-17 09:12:20 +0200

    Proper tests for to_float.

--- test/prop_basic_validation.erl
+++ test/prop_basic_validation.erl
@@ -1,7 +1,7 @@
 -module(prop_basic_validation).
 -include_lib("proper/include/proper.hrl").
 
--import(baleen, [validate/2, valid/0, invalid/0, to_integer/0]).
+-import(baleen, [validate/2, valid/0, invalid/0, to_integer/0, to_float/0]).
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
@@ -37,6 +37,30 @@ prop_is_integer_with_any_binary() ->
           catch
             error:badarg ->
               case baleen:validate(to_integer(), Binary) of
+                {error, _} ->
+                  true;
+                _ -> false
+              end
+          end).
+
+prop_is_float_with_float_strings() ->
+  ?FORALL(Float, float(),
+          {ok, Float} == baleen:validate(to_float(),
+                                         float_to_list(Float))).
+
+prop_is_float_with_ingeter_binaries() ->
+  ?FORALL(Float, float(),
+          {ok, Float} == baleen:validate(to_float(),
+                                         float_to_binary(Float))).
+
+prop_is_float_with_any_binary() ->
+  ?FORALL(Binary, binary(),
+          try
+            Float = binary_to_float(Binary),
+            {ok, Float} == baleen:validate(to_float(), Binary)
+          catch
+            error:badarg ->
+              case baleen:validate(to_float(), Binary) of
                 {error, _} ->
                   true;
                 _ -> false
