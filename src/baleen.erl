bc27f3c10df28dcdab89a7cd1d09828a175b6a45
MiguelERuiz <meruiz77@gmail.com>
2017-09-07 10:18:15 +0200

    All Eunit tests moved to test directory

--- src/baleen.erl
+++ src/baleen.erl
@@ -11,9 +11,6 @@
 %% @TODO complete the documentation
 -module(baleen).
 
-%% AH: we have some unit tests embedded in the implementation.
--include_lib("eunit/include/eunit.hrl").
-
 %% API exports
 
 %% Types
@@ -106,28 +103,10 @@ invalid() -> fun(X) ->
 		     {error, format("Invalid term ~p", [X])}
              end.
 
-invalid_simple_test_() ->
-    [ ?_assertMatch({error, <<"Invalid term",_/binary>>},
-		    validate(invalid(), "Hola"))
-    , ?_assertNotMatch({ok, _},
-		       validate(invalid(), 42)) ].
-
-invalid_test_() ->
-    Values = [true, null, undefined, "Hola", <<"">>, <<"Hola">>, 1, 0 , -1],
-    [ ?_assertMatch({error, <<"Invalid term",_/binary>>},
-		    validate(invalid(), Value))
-      || Value <- Values ].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec valid() -> validator(_,_).
 valid() ->
     fun(X) -> {ok, X} end.
-
-valid_test_() ->
-    Values = [true, null, undefined, "Hola", <<"">>, <<"Hola">>, 1, 0 , -1],
-    [ ?_assertMatch({ok, Value},
-		    validate(valid(), Value))
-      || Value <- Values ].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec to_integer() -> validator(str(), integer()).
@@ -144,16 +123,6 @@ to_integer() ->
 		_ -> {error, format("\"~w\" is not an integer", [Value])}
 	    end
     end.
-
-to_integer_test_() ->
-    Values = [123, 456, 3, 25, 76],
-    [?_assertEqual({ok, Value},
-		  validate(to_integer(), erlang:integer_to_binary(Value)))
-     || Value <- Values]
-	++
-	[?_assertEqual({ok, Value},
-		      validate(to_integer(), erlang:integer_to_list(Value)))
-	|| Value <- Values].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec compose(validator(A, B), validator(B, C)) -> validator(A, C).
@@ -190,20 +159,6 @@ all([V|Vs]) ->
 	    end
     end.
 
-all_test_() ->
-    Values = ["Hello", <<"By">>, 42],
-    [?_assertMatch({ok, Value},
-		   validate(all([valid(), valid()]), Value))
-     || Value <- Values]
-	++
-	[?_assertEqual({error, format("Invalid term ~p", [Value])},
-		       validate(all([valid(), invalid()]), Value))
-	 || Value <- Values]
-	++
-	[?_assertEqual({error, format("Invalid term ~p", [Value])},
-		       validate(all([invalid(), valid(), invalid()]), Value))
-	 || Value <- Values].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec any(list(validator(A, B))) -> validator(A, B).
 any([]) -> invalid();
@@ -221,24 +176,6 @@ any([V|Vs]) ->
 		    end
 	    end
     end.
-
-any_test_() ->
-    Values = ["Hello", <<"By">>, 42],
-    [?_assertMatch({ok, Value},
-		   validate(any([valid(), valid()]), Value))
-     || Value <- Values]
-	++
-	[?_assertMatch({ok, Value},
-		       validate(any([valid(), invalid()]), Value))
-	 || Value <- Values]
-	++
-	[?_assertNotMatch({error, <<"There isn't any valid">>},
-			  validate(any([invalid(), valid(), invalid()]), Value))
-	 || Value <- Values]
-	++
-	[?_assertMatch({error, <<"There isn't any valid">>},
-		       validate(any([invalid(), invalid(), invalid()]), Value))
-	 || Value <- Values].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec member(list(A)) -> validator(A, A).
@@ -262,32 +199,6 @@ literal(Term) ->
 	    end
     end.
 
-literal_test_() ->
-    Values = [undefined, 1, <<"Hello">>, true, 0, -1],
-    [?_assertMatch({ok, Value},
-		   validate(literal(Value), Value))
-     || Value <- Values]
-	++
-	[?_assertMatch({error, _},
-		       validate(literal(false), Value))
-	 || Value <- Values].
-
-literal_1_test_() ->
-    Terms = [4, undefined, <<"Bye">>, "foo"],
-    Values = [3, defined, <<"Hello">>, "bar"],
-    [?_assertEqual({error, format("~p and ~p do not match", [Term, Value])},
-		   validate(literal(Term), Value))
-     || Term <- Terms, Value <- Values].
-
-literal_2_test_() ->
-    Values = ["Hello", <<"By">>, 42],
-    [if Literal =:= Value ->
-	     ?_assertEqual({ok, Value}, validate(literal(Literal), Value));
-	true ->
-	     ?_assertMatch({error, _}, validate(literal(Literal), Value))
-     end
-     || Literal <- Values, Value <- Values].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec regex(String) -> validator(String, String) when String :: str().
 regex(RegularExpression) ->
@@ -308,43 +219,6 @@ regex(RegularExpression) ->
 	    throw(badarg)
     end.
 
-regex_test_() ->
-    Values = [<<"aab">>, <<"abababa">>],
-    [?_assertMatch({ok, Value},
-		   validate(regex(<<"[ab]*">>), Value))
-     || Value <- Values].
-
-regex_1_test_() ->
-    Values = [<<"ababab">>, <<"aaaaaaaaaaab">>],
-    REs = [<<"^c[ab]*$">>],
-    [?_assertEqual({error, format("~p is not matching the regular expression ~p", [Value, RE])},
-		   validate(regex(RE), Value))
-     || Value <- Values, RE <- REs].
-
-regex_2_test_() ->
-    Values = [<<"ababcdcd">>, <<"ab">>],
-    [?_assertMatch({ok, Value},
-		   validate(regex(<<"(ab)[cd]*">>), Value))
-     || Value <- Values].
-
-regex_3_test_() ->
-    Values = ["Hola"],
-    REs = [<<"[ab]*">>],
-    [?_assertEqual({error, format("~p is not matching the regular expression ~p", [Value, RE])},
-		   validate(regex(RE), Value))
-     || Value <- Values, RE <- REs].
-
-regex_4_test_() ->
-    Email_Regexp = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$",
-    Emails = ["aherranz@gmail.com", <<"angel.herranz@coowry.com">>],
-    No_Emails = ["1", "aherranz", <<"angel.herranz.coowry.com">>],
-    Email_Validator = regex(Email_Regexp),
-    [?_assertEqual({ok, Email}, validate(Email_Validator, Email))
-     || Email <- Emails ]
-	++
-	[?_assertMatch({error, _}, validate(Email_Validator, No_Email))
-	 || No_Email <- No_Emails ].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec max_length(non_neg_integer()) -> validator(S, S) when S :: iodata().
 max_length(I) -> 
@@ -354,16 +228,6 @@ max_length(I) ->
 		false -> {ok, S}
 	    end
     end.
-
-max_length_test_() ->
-    Values = [[<<"A tip">>], "Hello", <<"Bye">>],
-    [?_assertEqual({ok, Value},
-		   validate(max_length(140), Value))
-    || Value <- Values]
-	++
-	[?_assertMatch({error, _},
-		      validate(max_length(0), Value))
-	|| Value <- Values].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec to_atom() -> validator(str(), atom()).
@@ -382,16 +246,6 @@ to_atom() ->
 	    end
     end.
 
-to_atom_test_() ->
-    Atoms = ['1234', 'Hello', 'Bye', an_atom],
-    [?_assertEqual({ok, Atom},
-		   validate(to_atom(), erlang:atom_to_list(Atom)))
-     || Atom <- Atoms]
-	++
-	[?_assertEqual({ok, Atom},
-		       validate(to_atom(), erlang:atom_to_binary(Atom, utf8)))
-	 || Atom <- Atoms].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec list_of(validator(A,B)) -> validator(list(A), list(B)).
 list_of(V) ->
@@ -402,14 +256,6 @@ list_of(V) ->
 		Tuple -> {error, format("There was an error in a result: ~p", [Tuple])}
 	    end
     end.
-
-list_of_test_() ->
-    Values = ["1", "43", "86", "95"],
-    [?_assertEqual({ok, lists:map(fun(X) -> erlang:list_to_integer(X) end, Values)},
-		  validate(list_of(to_integer()), Values))]
-	++
-	[?_assertMatch({error, _},
-		      validate(list_of(to_atom()), Values))].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec map_of(validator(K1, K2), validator(V1, V2))
@@ -426,15 +272,6 @@ map_of(VK, VV) ->
 	    end
     end.	    
 
-map_of_test_() ->
-    Values = #{<<"Hello">> => "1234", <<"Bye">> => "5678"},
-    [?_assertEqual({ok, #{'Bye' => 5678, 'Hello' => 1234}},
-		 validate(map_of(to_atom(), to_integer()), Values))]
-     ++
-	[?_assertMatch({error, _},
-		      validate(map_of(to_integer(), to_atom()), Values))
-	].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec tuple_of(validator(A, B)) -> validator({A}, {B}).
 tuple_of(V) -> 
@@ -446,27 +283,12 @@ tuple_of(V) ->
 	    end
     end.
 
-tuple_of_test_() ->
-    Values = [<<"1">>, <<"Bye">>, <<"Hello">>, <<"atom">>],
-    [?_assertEqual({ok, erlang:list_to_tuple(lists:map(fun(X) -> erlang:binary_to_atom(X, utf8) end,Values))},
-		  validate(tuple_of(to_atom()), erlang:list_to_tuple(Values)))]
-	++
-	[?_assertMatch({error, _},
-		      validate(tuple_of(to_integer()),
-					erlang:list_to_tuple(Values)))].
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec transform(fun((A) -> B)) -> validator(A,B).
 transform(F) ->
     fun(T) ->
 	    {ok, F(T)}
     end.
-
-transform_test_() ->
-    Numbers = [1, 77, 23, 50, 95],
-    [?_assertEqual({ok, erlang:integer_to_list(Number)},
-		   validate(transform(fun erlang:integer_to_list/1), Number))
-     || Number <- Numbers].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec to_float() -> validator(str(), float()).
@@ -483,16 +305,6 @@ to_float() ->
 		_ ->  {error, ?INVALID_FLOAT(Value)}
 	    end
     end.
-		    
-to_float_test_() ->
-    Values = [3.14, 17.3e-6, 0.000000000001],
-    [?_assertEqual({ok, Value},
-		  validate(to_float(), erlang:float_to_binary(Value)))
-    || Value <- Values]
-	++
-	    [?_assertEqual({ok, Value},
-		  validate(to_float(), erlang:float_to_list(Value)))
-    || Value <- Values].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec val_map(#{K => {optional|required, validator(A,B)}},
@@ -520,34 +332,6 @@ val_map(Validator, Map) ->
       nonvalid => Invalids,
       missing => Missing,
       unexpected => Unexpected}.
-
-val_map_test_() ->
-    Validator = #{msisdn => {required, regex("^\\+[1-9][0-9]{8,14}$")},
-		  email => {optional, regex("^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$")}
-		 },
-    Info = #{email => "meruiz@email.com",
-	     field => "Some description"},
-    ?_assertEqual(#{valid => #{email => "meruiz@email.com"},
-		    nonvalid => #{},
-		    missing => [msisdn],
-		    unexpected => [field]},
-		 val_map(Validator, Info)).
-
-val_map_1_test_() ->
-    EmailRegex = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$",
-    Validator = #{msisdn => {required, regex("^\\+[1-9][0-9]{8,14}$")},
-		  email => {optional, regex(EmailRegex)},
-		  concept => {optional, compose(literal("A tip for your service"), to_atom())}
-		 },
-    Info = #{msisdn => "+34666657231",
-	     email => "meruiz.email.com",
-	     concept => "A tip for your service"},
-    ?_assertEqual(#{valid => #{msisdn => "+34666657231",
-			       concept => 'A tip for your service'},
-		    nonvalid => #{email => format("~p is not matching the regular expression ~p", ["meruiz.email.com", EmailRegex])},
-		    missing => [],
-		    unexpected => []},
-		 val_map(Validator, Info)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%====================================================================
